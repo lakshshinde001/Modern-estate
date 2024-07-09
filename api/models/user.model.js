c484b98311ca97812bdb5f045d681af33aa115c7
lakshshinde001 <laxmanshinde3105@gmail.com>
2024-07-09 23:45:17 +0530

    Created sign Up Route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,22 +1,26 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    passoword: {
-        type: String,
-        required: true,
-    }
-}, { timestamps: true });
 
-const User = mongoose.model('User', userSchema);
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+          },
+        },
+        { timestamps: true }
+      );
 
-export default User;
+      const User = mongoose.model('User', userSchema);
+
+      export default User;
