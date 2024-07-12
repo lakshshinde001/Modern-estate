d5252810db3aa2ad256496816c84077d37d62258
lakshshinde001 <laxmanshinde3105@gmail.com>
2024-07-12 22:35:32 +0530

    Added Sign out and delete user facility

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -32,14 +32,21 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to='/profile'>
+          {/* <Link to='/profile'>
             {currentUser ? (
               <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
             ) : (
               
               <li className=' text-slate-700 hover:underline'> Sign in</li>
             )}
-          </Link>
+          </Link> */}
+         {
+          currentUser ?  ( <Link to='/profile'>
+             <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
+          </Link>) : (<Link to= '/sign-in'>
+            <li className=' text-slate-700 hover:underline'> Sign in</li>
+          </Link>)
+         }
 
         </ul>
       </div>
