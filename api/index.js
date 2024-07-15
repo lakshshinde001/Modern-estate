4bae09171d69194fe75c2d8b2bd9a0b1e99e8ed9
lakshshinde001 <laxmanshinde3105@gmail.com>
2024-07-15 17:51:25 +0530

    Home page finished & fixed some bugs

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listings.route.js'
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
+import path from 'path';
 
 
 dotenv.config();
@@ -20,6 +21,9 @@ mongoose
     console.log(err);
   });
 
+  const __dirname = path.resolve();
+
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -32,6 +36,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-500'>Modern</span>
             <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
@@ -57,7 +57,7 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to='/profile'>
+          {/* <Link to='/profile'>
             {currentUser ? (
               <img
                 className='rounded-full h-7 w-7 object-cover'
@@ -67,7 +67,23 @@ export default function Header() {
             ) : (
               <li className=' text-slate-700 hover:underline'> Sign in</li>
             )}
-          </Link>
+          </Link> */}
+
+        {currentUser ? (
+          <Link to='/profile'>
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            </Link>
+            ) : (
+              <Link to='/sign-in'>
+                <li className=' text-slate-700 hover:underline'> Sign in</li>
+              </Link>
+            )}
+          
+
         </ul>
       </div>
     </header>
